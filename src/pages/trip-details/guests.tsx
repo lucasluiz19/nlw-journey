ef7e009eb14c9eef2504cd372ab723bcd420fd1f
Lucas Luiz Martins da Silva <lucasluiz@MacBook-Pro-de-Lucas.local>
2024-09-17 21:45:27 -0300

    creating connection api guests

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,34 +1,53 @@
-import { CircleDashed, CircleCheck, UserCog } from "lucide-react";
+import { CircleDashed,  UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+interface Participant {
+  id:string
+  name:string | null
+  email:string
+  is_confirmed: boolean
+
+} 
 
 export function Guests(){
+
+    const {tripsId} =useParams()
+    const [participants, setParticipants]= useState<Participant[]>([])
+
+    useEffect(()=>{
+      api.get(`/trips/${tripsId}/participants`).then(response => setParticipants(response.data.participants  ))
+    },[tripsId])
+
     return(
      <div className="space-y-6" >
         <h2 className="font-semibold text-xl  ">Convidados</h2>
 
-        <div className="space-y-5">
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
-              <span className="block text-sm text-zinc-400  ">
-                lacy.stiedemann@gmail.com
-              </span>
-            </div>                  
-            <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
-          </div>
+        
+        {participants.map(participant =>{
+            return(
+              <div key={participant.id} className="space-y-5">
+                <div  className="flex items-center justify-between gap-4">
+                  <div className="space-y-1.5">
+                    <span className="block font-medium text-zinc-100">{participant.name}</span>
+                    <span className="block text-sm text-zinc-400  ">
+                      {participant.email}
+                    </span>
+                  </div>                  
+                  <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+                </div>  
+              </div> 
+            )
+          }
+         )
+        }
 
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="block font-medium text-zinc-100">Jessica White</span>
-              <span  className="block text-sm text-zinc-400  ">
-                jessica.white44@yahoo.com
-              </span>
-            </div>                  
-            <CircleCheck className="text-lime-300 size-5 shrink-0"/>
-          </div>
+         
 
 
-        </div>
+      
 
         <Button size="full" variant="secondary">
           Gerenciar convidados
@@ -38,3 +57,5 @@ export function Guests(){
       </div>  
     )
 }
+
+
